fb0693fee0db0fc414ba74373394891228d52d92
Pwanedo Philip <ppkuros12@gmail.com>
2021-06-02 20:32:38 +0100

    A home page is been created

---
+++ home.php
@@ -0,0 +1,30 @@
+
+<html>
+
+<head>
+</head>
+
+<body>
+
+<h1><b><center>WELCOME TO WEB DEVELOPMENT</center></b></h1>
+<center>Please login...</center>
+
+<div align="center">
+<form method="post" action="login.php">
+<table border="1" width="25%">
+<tr><td width="10%">Name: </td><td><input type="text" name="name" maxlength="20" /></td></tr><br /><p>
+<tr><td width="10%">Password: </td><td><input type="password" name="password" maxlength="20" /></td></tr><br />
+</table>
+ <p>
+<input type="submit" value="Login" /><br />
+</form>
+<a href="form.php">Register?</a>
+</center>
+
+<h2><center>
+<?php include("links.php"); ?>
+</center></h2>
+</body>
+
+
+</html>
