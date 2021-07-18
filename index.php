360a4030501fb0d7b73899962ff7ceac74ca6484
PWANEDO PHILIP <83373586+pp199142@users.noreply.github.com>
2021-07-18 19:27:21 +0100

    Index page for user to see his/her post

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php 
+  session_start();
+  if(isset($_SESSION['unique_id'])){
+    header("location: users.php");
+  }
+?>
+
+<?php include_once "header.php"; ?>
+<body>
+  <div class="wrapper">
+    <section class="form signup">
+      <header>My Chatting App</header>
+     
+	 
+	 
+	 
+      <div class="link">Already signed up? <a href="login.php">Login now</a></div>
+    </section>
+  </div>
+
+  <script src="javascript/pass-show-hide.js"></script>
+  <script src="javascript/signup.js"></script>
+
+</body>
+</html>
