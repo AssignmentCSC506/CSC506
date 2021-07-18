b678c1102966463594aa7d2717ea7ab7dda9ebfb
PWANEDO PHILIP <83373586+pp199142@users.noreply.github.com>
2021-07-18 19:21:43 +0100

    Login page for making a post and comment

---
+++ login.php
@@ -0,0 +1,27 @@
+<?php 
+  session_start();
+  if(isset($_SESSION['unique_id'])){
+    header("location: users.php");
+  }
+?>
+
+<?php include_once "header.php"; ?>
+<body>
+  <div class="wrapper">
+    <section class="form login">
+      <header align="center">My Chatting Application</header>
+   
+   
+   
+   
+   
+   
+      <div class="link">Not yet signed up? <a href="index.php">Signup now</a></div>
+    </section>
+  </div>
+  
+  <script src="javascript/pass-show-hide.js"></script>
+  <script src="javascript/login.js"></script>
+
+</body>
+</html>
