26c439cd27df67786cf2817abc887fb084ae2aaa
PWANEDO PHILIP <83373586+pp199142@users.noreply.github.com>
2021-07-18 19:39:40 +0100

    Database connection which will retrieve user.

--- users.php
+++ users.php
@@ -12,7 +12,17 @@
       <header>
         <div class="content">
        
-        
+        	  <?php 
+            $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$_SESSION['unique_id']}");
+            if(mysqli_num_rows($sql) > 0){
+              $row = mysqli_fetch_assoc($sql);
+            }
+          ?>
+          <img src="php/images/<?php echo $row['img']; ?>" alt="">
+          <div class="details">
+            <span><?php echo $row['fname']. " " . $row['lname'] ?></span>
+            <p><?php echo $row['status']; ?></p>
+          </div>
 		
 		
         </div>
